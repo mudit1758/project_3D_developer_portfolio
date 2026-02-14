27de7052906d30c1cc4827d9abe74fc0a8ed4929
mudit1758 <mudit.raj10@gmail.com>
2026-02-14 14:56:15 +0530

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = parseInt(process.env.VITE_PORT || process.env.PORT) || 5173;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: port,
+  },
 })
